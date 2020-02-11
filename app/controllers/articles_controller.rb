f7e03eae1dc8e2b967b734efea79919e59202b4b
julien laloum <djulesdev@gmail.com>
2020-02-11 22:27:44 +0100

    RAILS DAYS 2 EXO 3 not finish

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,30 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @articles = Article.find(params[:id])
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
